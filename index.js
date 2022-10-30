98a4c0ab2b621a310c4c22c6bee6eeb4eaffc63a
AnastasiiaBeltsova <anastasiya.b145@gmail.com>
2022-10-30 20:48:59 +0200

    init comit

---
+++ index.js
@@ -0,0 +1,28 @@
+jQuery(document).ready(function ($) {
+    let currentImg;
+    let clickedImg;
+    $('.gallery .js--gal_item').click(function () {
+        // let src = $(this).find('img').attr('src');
+        // currentImg = $('.js--modal__content img').attr('src', src);
+        currentImg = $('.js--modal__content img').attr('src', $(this).find('img').attr('src'));
+        $('.js--modal').fadeIn();
+        clickedImg = $(this);
+    })
+    $('.js--modal__close').click(function () {
+        $('.js--modal').fadeOut();
+    })
+
+    $('.js--modal__next').click(function () {
+        currentImg.fadeOut(function () {
+            clickedImg.next().trigger('click');
+            currentImg.fadeIn(); 
+        })
+    })
+
+    $('.js--modal__prev').click(function () {
+        currentImg.fadeOut(function () {
+            clickedImg.prev().trigger('click');
+            currentImg.fadeIn();
+        })
+    })
+})
