98248342ff4f1729e5e5ee68e43e64b6a76b7df9
AnastasiiaBeltsova <102054895+AnastasiiaBeltsova@users.noreply.github.com>
2022-10-30 23:02:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ jQuery(document).ready(function ($) {
     let currentImg;
     let clickedImg;
     $('.gallery .js--gal_item').click(function () {
-        // let src = $(this).find('img').attr('src');
-        // currentImg = $('.js--modal__content img').attr('src', src);
         currentImg = $('.js--modal__content img').attr('src', $(this).find('img').attr('src'));
         $('.js--modal').fadeIn();
         clickedImg = $(this);
     })
+    
     $('.js--modal__close').click(function () {
         $('.js--modal').fadeOut();
     })
